5db1b6a4c3562d5c1b2edbe1f8b0f0fdb232e481
Moses <Moses@localhost>
2015-12-15 04:34:38 -0500

    Added assertion to make sure a definition has a valid self.Endian Added check to make sure the TYPE in a descriptor is a valid Field_Type when sanitizing Added an early return if a dict doesnt have a TYPE in it Added Switch type(needs properly written Reader routine to work) Added Is_Enum and Is_Bool properties to Field_Types Added enumerator and boolean integers of 8/16/32/64 bit sizes and bit/big versions Added Str_Latin1_Enum for use in places where a fixed length string is used as an     identifier in a structure, but it is only valid to set it to a certain set of options Replaced all instances of ELEMENTS and FLAGS with OPTIONS Replaced all instances of ATTR_OFFSETS with ATTR_OFFS Each Field_Type with ELEMENTS was replaced with an equivalent enumerator Field_Type Each Field_Type with FLAGS was replaced with an equivalent boolean Field_Type Tweaked TGA struct and descriptors found in Common_Structures Changed how flags and enumerators are calculated in the descriptor sanitizing Removed ELEMENTS and FLAGS from Constants.py and created OPTIONS Removed ALIGN_MODES since I don't see it ever being useful Removed commented out chunk of code from descriptor _Sanitize_Name()

--- Defs/bitmaps/tga.py
+++ Defs/bitmaps/tga.py
@@ -73,46 +73,44 @@ class TGA_Def(Tag_Def):
     
     Tag_Structure = { TYPE:Struct, GUI_NAME:"TGA Image",
                       0:{ TYPE:UInt8, GUI_NAME:"Image ID Length"},
-                      1:{ TYPE:UInt8, GUI_NAME:"Has Color Map",
-                          ELEMENTS:{0:{NAME:"No"},
-                                    1:{NAME:"Yes"}
-                                    }
+                      1:{ TYPE:Enum8, GUI_NAME:"Has Color Map",
+                          OPTIONS:{0:{NAME:"No"},
+                                   1:{NAME:"Yes"}
+                                   }
                           },
                       2:{ TYPE:Bit_Struct, GUI_NAME:"Image Type",
-                           0:{TYPE:Bit_UInt, GUI_NAME:"Format", SIZE:2,
-                              ELEMENTS:{0:{GUI_NAME:"BW 1 Bit"},
-                                        1:{GUI_NAME:"Color Mapped RGB"},
-                                        2:{GUI_NAME:"Unmapped RGB"},
-                                        }
+                           0:{TYPE:Bit_Enum, GUI_NAME:"Format", SIZE:2,
+                              OPTIONS:{0:{GUI_NAME:"BW 1 Bit"},
+                                       1:{GUI_NAME:"Color Mapped RGB"},
+                                       2:{GUI_NAME:"Unmapped RGB"},
+                                       }
                               },
                            1:{ PAD:1 },
-                           2:{TYPE:Bit_UInt, GUI_NAME:"RLE Compressed", SIZE:1,
-                              ELEMENTS:{0:{NAME:"No"}, 1:{NAME:"Yes"} }
+                           2:{TYPE:Bit_Enum, GUI_NAME:"RLE Compressed", SIZE:1,
+                              OPTIONS:{0:{NAME:"No"}, 1:{NAME:"Yes"} }
                               }
                           },
                       3:{ TYPE:UInt16, GUI_NAME:"Color Map Origin" },
                       4:{ TYPE:UInt16, GUI_NAME:"Color Map Length" },
                       5:{ TYPE:UInt8, GUI_NAME:"Color Map Depth" },
-                      6:{ TYPE:Struct, GUI_NAME:"Image Origin",
-                          0:{ TYPE:UInt16, NAME:"X" },
-                          1:{ TYPE:UInt16, NAME:"Y" },
-                          },
-                      7:{ TYPE:UInt16, NAME:"Width" },
-                      8:{ TYPE:UInt16, NAME:"Height" },
-                      9:{ TYPE:UInt8, NAME:"BPP" },
-                      10:{ TYPE:Bit_Struct, GUI_NAME:"Image Descriptor",
+                      6:{ TYPE:UInt16, GUI_NAME:"Image Origin X" },
+                      7:{ TYPE:UInt16, GUI_NAME:"Image Origin Y" },
+                      8:{ TYPE:UInt16, NAME:"Width" },
+                      9:{ TYPE:UInt16, NAME:"Height" },
+                      10:{ TYPE:UInt8, NAME:"BPP" },
+                      11:{ TYPE:Bit_Struct, GUI_NAME:"Image Descriptor",
                            0:{TYPE:Bit_UInt, GUI_NAME:"Alpha Bit Count", SIZE:4},
                            1:{ PAD:1 },
-                           2:{TYPE:Bit_UInt, GUI_NAME:"Screen Origin", SIZE:1,
-                              ELEMENTS:{0:{GUI_NAME:"Lower Left"},
-                                        1:{GUI_NAME:"Upper Left"}
-                                        },
+                           2:{TYPE:Bit_Enum, GUI_NAME:"Screen Origin", SIZE:1,
+                              OPTIONS:{0:{GUI_NAME:"Lower Left"},
+                                       1:{GUI_NAME:"Upper Left"}
+                                       },
                               },
-                           3:{TYPE:Bit_UInt, GUI_NAME:"Interleaving", SIZE:2,
-                              ELEMENTS:{0:{GUI_NAME:"None"},
-                                        1:{GUI_NAME:"Two Way"},
-                                        2:{GUI_NAME:"Four Way"},
-                                        }
+                           3:{TYPE:Bit_Enum, GUI_NAME:"Interleaving", SIZE:2,
+                              OPTIONS:{0:{GUI_NAME:"None"},
+                                       1:{GUI_NAME:"Two Way"},
+                                       2:{GUI_NAME:"Four Way"},
+                                       }
                               }
                           },
                      CHILD:{ TYPE:Container, NAME:"Image_Data",
