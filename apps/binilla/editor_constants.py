1d904ed533620f201b7707b75f8b8850c3919398
Moses <Moses@localhost>
2016-12-02 01:30:26 -0500

    Implemented config file for binilla Finished fixing ScrollMenu bugs(that i'm aware of)

--- apps/binilla/editor_constants.py
+++ apps/binilla/editor_constants.py
@@ -1,15 +1,15 @@
 from supyr_struct.defs.constants import *
 
 # padding to use when packing a widget being oriented vertically
-VERTICAL_FRAME_PADX = (20, 0)
-VERTICAL_FRAME_PADY = (0, 5)
+VERTICAL_PADX = (20, 0)
+VERTICAL_PADY = (0, 5)
 
 # padding to use when packing a widget being oriented horizontally
 HORIZONTAL_PADX = (0, 10)
 HORIZONTAL_PADY = (0, 5)
 
 # The default text width of the title label for widgets
-FRAME_TITLE_WIDTH = 35
+TITLE_WIDTH = 35
 
 # default depths for each of the different widget types
 COMMENT_DEPTH = 1
@@ -20,20 +20,20 @@ FRAME_DEPTH = 3
 WHITE = '#%02x%02x%02x' % (255, 255, 255)
 BLACK = '#%02x%02x%02x' % (0, 0, 0)
 
-DEFAULT_BG_COLOR = '#%02x%02x%02x' % (236, 233, 216)  # light tan
-TOOLTIP_BG_COLOR = '#%02x%02x%02x' % (241, 239, 226)  # lighter tan
-FRAME_BG_COLOR = '#%02x%02x%02x' % (172, 168, 153)  # muddy tan
 IO_BG_COLOR = '#%02x%02x%02x' % (47, 47, 50)  # dark grey
 IO_FG_COLOR = '#%02x%02x%02x' % (195, 195, 200)  # very light grey
+DEFAULT_BG_COLOR = '#%02x%02x%02x' % (236, 233, 216)  # light tan
+COMMENT_BG_COLOR = '#%02x%02x%02x' % (241, 239, 226)  # lighter tan
+FRAME_BG_COLOR = '#%02x%02x%02x' % (172, 168, 153)  # muddy tan
 
 TEXT_NORMAL_COLOR = BLACK
 TEXT_DISABLED_COLOR = FRAME_BG_COLOR
 TEXT_SELECTED_COLOR = DEFAULT_BG_COLOR
 TEXT_HIGHLIGHTED_COLOR = '#%02x%02x%02x' % (55, 110, 210)  # pale lightish blue
 
-ENUM_BG_NORMAL_COLOR = WHITE
-ENUM_BG_DISABLED_COLOR = DEFAULT_BG_COLOR
-ENUM_BG_SELECTED_COLOR = TEXT_HIGHLIGHTED_COLOR
+ENUM_NORMAL_COLOR = WHITE
+ENUM_DISABLED_COLOR = DEFAULT_BG_COLOR
+ENUM_SELECTED_COLOR = TEXT_HIGHLIGHTED_COLOR
 
 
 # The number of text units wide the ScrollMenu is at a minimum
