dacd1d30a7c58c7426400878b2d04f6ee9ad1624
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-10-25 17:33:42 -0400

    Additions to PeekableMmap to help manage memory better Misc tweaks and fixes Made speed of pad_idat_data significantly faster Allowed specifying compression level in set_chunk_data Added trns_chunk to png definition

--- defs/bitmaps/objs/png.py
+++ defs/bitmaps/objs/png.py
@@ -9,9 +9,12 @@ from supyr_struct.tag import *
 IDAT_CHUNK_SIG = int.from_bytes(b'IDAT', 'big')
 
 def pad_idat_data(data, stride):
-    new_data = b''
+    # this is the fastest way to do this
+    new_data = bytearray(((stride + 1)*len(data))//stride)
+    pad = 1
     for i in range(0, len(data), stride):
-        new_data += b'\x00' + data[i: i+stride]
+        new_data[i+pad: i+pad+stride] = data[i: i+stride]
+        pad += 1
 
     return new_data
 
@@ -37,7 +40,7 @@ class PngTag(Tag):
             return zlib.decompress(chunk[-2])  # second to last thing is data
         return None
 
-    def set_chunk_data(self, chunk, new_data):
+    def set_chunk_data(self, chunk, new_data, png_compress_level=None):
         if chunk.sig == IDAT_CHUNK_SIG:
             compression = chunk.parent[0].compression
         elif hasattr(chunk, "compression"):
@@ -46,9 +49,13 @@ class PngTag(Tag):
             raise TypeError("This chunk cannot contain compressed raw data.")
 
         if compression.enum_name == "deflate":
-            chunk[-2] = zlib.compress(new_data)
+            if png_compress_level in range(1, 7):
+                chunk[-2] = zlib.compress(new_data, png_compress_level)
+            else:
+                chunk[-2] = zlib.compress(new_data)
 
     def serialize(self, *args, **kwargs):
-        for chunk in self.data.chunks:
-            self.calculate_chunk_checksum(chunk)
+        if kwargs.get("calc_checksums", True):
+            for chunk in self.data.chunks:
+                self.calculate_chunk_checksum(chunk)
         Tag.serialize(self, *args, **kwargs)
