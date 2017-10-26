1cf565071e1ff11c8f1ebb2d98b60c9baf6f6129
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2017-10-26 19:43:54 -0400

    Made it so OSError's still get through, even when allowing corrupt tags

--- supyr_struct/setup.py
+++ supyr_struct/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2017.10.25"
-version = (1, 0, 5)
+release_date = "2017.10.26"
+version = (1, 0, 6)
 
 try:
     try:
