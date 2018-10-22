07146575941e4e094579f04b148a1e8194d9941f
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2018-10-22 02:57:17 -0400

    Version bump

--- supyr_struct/setup.py
+++ supyr_struct/setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.09.30"
-version = (1, 1, 4)
+release_date = "2018.10.22"
+version = (1, 1, 5)
 
 try:
     try:
