035db10a2d093f0ed58246cc09709924b07b7de4
Moses_of_Egypt <29311834+MosesofEgypt@users.noreply.github.com>
2019-01-19 00:01:55 -0500

    Implemented hashing for FrozenDict     Not sure how good the algorithm is right now, but it should hold up

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ except ImportError:
 curr_dir = dirname(__file__)
 
 #               YYYY.MM.DD
-release_date = "2018.12.24"
-version = (1, 1, 8)
+release_date = "2019.01.18"
+version = (1, 1, 9)
 
 try:
     try:
