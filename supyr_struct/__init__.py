2e68b76d231b2f2fa02d9c730598faff943bbcfe
gbMichelle <gbmichelle.dev@gmail.com>
2020-02-08 00:16:59 +0100

    Bump version

--- supyr_struct/__init__.py
+++ supyr_struct/__init__.py
@@ -88,7 +88,7 @@ from supyr_struct import buffer, blocks, exceptions, field_types, util, tag
 __author__ = "Devin Bobadilla, Michelle van der Graaf"
 #           YYYY.MM.DD
 __date__ = "2020.02.10"
-__version__ = (1, 5, 0)
+__version__ = (1, 5, 1)
 __website__ = "https://github.com/Sigmmma/supyr_struct"
 
 
