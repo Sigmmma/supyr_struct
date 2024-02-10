a2a867817a2cadfe6a89aed10dc1efaa90cce583
Steven Garcia <stevengarcia55@ymail.com>
2024-02-10 13:47:46 -0600

    bump version

--- supyr_struct/__init__.py
+++ supyr_struct/__init__.py
@@ -87,8 +87,8 @@ from supyr_struct import buffer, blocks, exceptions, field_types, util, tag
 # ##############
 __author__ = "Sigmmma"
 #           YYYY.MM.DD
-__date__ = "2024.01.23"
-__version__ = (1, 5, 5)
+__date__ = "2024.02.10"
+__version__ = (1, 5, 6)
 __website__ = "https://github.com/Sigmmma/supyr_struct"
 
 
